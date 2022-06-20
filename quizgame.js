2872f7274d4e598fe1c055f3e7eb0c4d5f4bb51f
tiinaVaisanen <tiina.vaisanen@awacademy.training>
2022-06-20 16:19:49 +0300

    täspä tää

--- quizgame.js
+++ quizgame.js
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+console.log("Hello World!");
+
+const prompt = require("prompt-sync")(); // määritetään prompt-moduuli
+
+var thirdQuestion = prompt("What is the capital of France?");
+if (thirdQuestion == "Paris") {
+    score = score + 5;
+    confirm("You are right!");
+    console.log("Your score is: " + score);
+} else {
+    score = score - 5;
+    confirm("You are wrong");
+    console.log("Your score is: " + score);
+}
+
+switch (score) {
+    case 0:
+        alert("You lose!");
+        break;
+    case 30:
+        alert("You win!");
+        break;
+    default:
+        alert("Something went wrong!");
+        break;
