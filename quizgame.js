406dc0e21b109b147d09d060f744fc4fea988fb4
AlexK <alex.kiskola@awacademy.training>
2022-06-20 15:49:34 +0300

    Quiz Game pt. 2

--- quizgame.js
+++ quizgame.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+var score = 15;
+
+while (score > 0 && score < 30) {
+    var firstQuestion = prompt("What's the capital of Sweden?");
+    if (firstQuestion == "Stockholm") {
+        score = score + 5;
+        confirm("You are right!");
+        console.log("Your score is: " + score);
+    } else {
+        score = score - 5;
+        confirm("You are wrong");
+        console.log("Your score is: " + score);
+    }
+
+    var secondQuestion = prompt("What's the capital of Thailand?");
+    if (secondQuestion == "Bangkok") {
+        score = score + 5;
+        confirm("You are right!");
+        console.log("Your score is: " + score);
+    } else {
+        score = score - 5;
+        confirm("You are wrong");
+        console.log("Your score is: " + score);
+    }
